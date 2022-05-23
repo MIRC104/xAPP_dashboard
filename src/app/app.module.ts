43f044d3dbb149d5cff8d84911a9cb5d7f05b062
pinyun huang <huangp585@gmail.com>
2022-05-23 06:09:02 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TagsComponent } from './tags/tags.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ImagehistoryComponent } from './imagehistory/imagehistory.component';
+import { FrontPageComponent } from './front-page/front-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TagsComponent,
+    ImagehistoryComponent,
+    FrontPageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
